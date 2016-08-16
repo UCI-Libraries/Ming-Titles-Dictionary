42b7da30dea3537131fc72a89f60d0d41fb90690
clwoods <clwoods@uci.edu>
2016-08-15 23:20:19 -0700

    init passing current user to controller

--- mingTitles/app/assets/javascripts/angular/controllers/main.js
+++ mingTitles/app/assets/javascripts/angular/controllers/main.js
@@ -4,9 +4,20 @@ titlesApp.controller('mainController', ['$scope', '$timeout', function($scope, $
   // var start = angular.element(document.getElementsByClassName('new-game'));
 
   $scope.title = "Ming Titles";
+  $scope.current_user = "foo";
   $scope.badGuesses = 0;
   $scope.resetHangman = false;
   $scope.resetAlpha = false;
+  $scope.init = function(str) {
+    console.log(str);
+    logSomeStuff(str);
+  };
+
+  function logSomeStuff(str) {
+    $scope.current_user = str;
+  }
+
+  $scope.$evalAsync(logSomeStuff());
 
 
 }]);
