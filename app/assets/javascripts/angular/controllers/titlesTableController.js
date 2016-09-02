2b57e21b1a15ecef2e38114ade8411d5cba9be2d
clwoods <clwoods@uci.edu>
2016-09-01 17:45:57 -0700

    table sorts by number of translations

--- app/assets/javascripts/angular/controllers/titlesTableController.js
+++ app/assets/javascripts/angular/controllers/titlesTableController.js
@@ -1,7 +1,6 @@
 titlesApp
   .controller('titlesTableController', ['$scope', '$timeout', '$http', 'NgTableParams', function($scope, $timeout, $http, NgTableParams){
   // var addItem = angular.element(document.getElementsByClassName('add-item'));
-  $scope.titles = [];
   $scope.tableParams = new NgTableParams({}, { dataset: data});
 
   var init = function() {
@@ -11,7 +10,10 @@ titlesApp
   function getTitles() {
       $http.get('titles').then(function(response) {
         console.log(response.data);
-        $scope.titles = response.data;
+        for (var i = 0; i < response.data.length; i++) {
+          response.data[i].num_translations = 0; //initialization of new property
+          response.data[i].num_translations = response.data[i].translations.length;  //set the data from nested obj into new property
+        }
         $scope.tableParams = new NgTableParams({}, { dataset: response.data});
 
       });
