54b098236dcf8f09e41c5eb9eb9a1f347ccae9a4
clwoods <clwoods@uci.edu>
2016-10-19 14:01:30 -0700

    flagged added back to translations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010040750) do
+ActiveRecord::Schema.define(version: 20161019204656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20161010040750) do
     t.datetime "comment_added_at"
     t.boolean  "flag",               default: false
     t.boolean  "reviewed",           default: false
+    t.boolean  "flagged",            default: false
     t.index ["title_id"], name: "index_translations_on_title_id", using: :btree
     t.index ["user_id"], name: "index_translations_on_user_id", using: :btree
   end
