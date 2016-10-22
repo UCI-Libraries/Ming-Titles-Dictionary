dc98d1b56b10aff3316d2be9f6165996e130034c
clwoods <clwoods@uci.edu>
2016-10-22 12:20:41 -0700

    params updated in controller for user profile info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -131,6 +131,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {}).permit(:approved, :id, :source, :has_contributed, :password, :password_confirmation)
+      params.fetch(:user, {}).permit(:approved, :id, :source, :has_contributed, :password, :password_confirmation, :email, :institution, :country, :fname, :lname, :research)
     end
 end
