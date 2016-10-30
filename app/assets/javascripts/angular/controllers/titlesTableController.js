506a8af01ffacda50b84f2eee67b21b1b3d67aee
clwoods <clwoods@uci.edu>
2016-10-29 18:09:25 -0700

    titles cacheing on front end using titleService

--- app/assets/javascripts/angular/controllers/titlesTableController.js
+++ app/assets/javascripts/angular/controllers/titlesTableController.js
@@ -15,23 +15,40 @@ titlesApp
   };
 
   function getTitles(inst) {
-    var route = 'api/titles/';
-    if(!!inst || inst === '') {
-      route = 'api/titles/institution/' + inst;
-    }
-    $scope.loading = true;
-
-    $http.get(route).then(function(response) {
-      // console.log(response.data);
-      for (var i = 0; i < response.data.length; i++) {
-        response.data[i].num_translations = 0; //initialization of new property
-        // console.log(response.data[i].translations);
-
-        response.data[i].num_translations = response.data[i].translations.length;  //set the data from nested obj into new property
+    if (!!inst || inst === '') {
+      if (inst > 0 && titlesService.titles[inst]) {
+        $scope.tableParams.settings({dataset: titlesService.titles[inst]});
+      } else {
+        var titles_by_inst_route = 'api/titles/institution/' + inst;
+        $scope.loading = true;
+        $http.get(titles_by_inst_route).then(function(response) {
+          for (var i = 0; i < response.data.length; i++) {
+            response.data[i].num_translations = 0; //initialization of new property
+            response.data[i].num_translations = response.data[i].translations.length;  //set the data from nested obj into new property
+          }
+          titlesService.titles[inst] = response.data;
+          $scope.tableParams.settings({dataset: response.data});
+          $scope.loading = false;
+        });
       }
-      $scope.tableParams.settings({dataset: response.data});
-      $scope.loading = false;
-    });
+    } else {
+      if (titlesService.titles.all) {
+        $scope.tableParams.settings({dataset: titlesService.titles.all});
+      } else {
+        $scope.loading = true;
+        var titles_route = 'api/titles/';
+
+        $http.get(titles_route).then(function(response) {
+          for (var i = 0; i < response.data.length; i++) {
+            response.data[i].num_translations = 0; //initialization of new property
+            response.data[i].num_translations = response.data[i].translations.length;  //set the data from nested obj into new property
+          }
+          titlesService.titles.all = response.data;
+          $scope.tableParams.settings({dataset: response.data});
+          $scope.loading = false;
+        });
+      }
+    }
   }
 
   $scope.$on('updateTitleTable', function() {
