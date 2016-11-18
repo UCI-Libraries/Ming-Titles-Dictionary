b3b5bd7bce9fce88ca947c0a62fc14fa9546e6be
ucilibraries <ucilibraries@users.noreply.github.com>
2016-11-18 13:42:09 -0800

    correct version of mailer

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -4,7 +4,10 @@ class MyMailer < Devise::Mailer
   #include Devise::Controllers # Optional. eg. `confirmation_url`
   include Devise::Mailers::Helpers
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
-  default from: 'mingtitles@lib.uci.edu'
+  #AC 10312016 default from: 'mingtitles@lib.uci.edu'
+  default from: 'mingtitles@cygnus.lib.uci.edu'
+  #AC 11012016 default from: 'devnull@cygnus.lib.uci.edu'
+  #AC 11042016 default from: 'devnull@cygnus.lib.uci.edu'
 
   def greeting(id)
     @user = User.find_by_id(id)
@@ -28,6 +31,11 @@ class MyMailer < Devise::Mailer
     puts @translation.translation_text
     puts @translation.user
     mail(to: @user.email, subject: 'New Translation Submitted')
+    #AC 11042016 hardcoded 2 other superadmins
+    #@user = User.find_by_id(6)
+    #mail(to: @user.email, subject: 'New Translation Submitted')
+    #@user = User.find_by_id(7)
+    #mail(to: @user.email, subject: 'New Translation Submitted')
   end
 
   def notify_superadmin_new_scholar(id)
@@ -35,11 +43,26 @@ class MyMailer < Devise::Mailer
     @admin = User.find_by_id(1)
     @scholar =  User.find_by_id(id)
     mail(to: @admin.email, subject: 'New Scholar Application')
+    #AC 11042016 hardcoded 2 other superadmins	
+#    @admin = User.find_by_id(6)
+ #   @scholar =  User.find_by_id(id)
+ #   mail(to: @admin.email, subject: 'New Scholar Application')
+ #   @admin = User.find_by_id(7)
+ #   @scholar =  User.find_by_id(id)
+ #   mail(to: @admin.email, subject: 'New Scholar Application')
   end
 
   def acceptance(user)
     @user = user
     mail(to: @user.email, subject: 'Congratulations! You have been accepted as a contributing scholar.')
   end
+
+#AC 11042016 trying to send password reset email
+
+  #def change_password(user)
+  #  @user = user
+  #  mail(to: @user.email, subject: 'Password reset complete.')
+  #end
+
 
 end
