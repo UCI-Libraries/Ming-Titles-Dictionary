888e6268db117ce1d5e94a6a25d553d600b523ea
ucilibraries <ucilibraries@users.noreply.github.com>
2016-11-18 13:38:00 -0800

    new file to allow new Bibliography menu item

---
+++ app/assets/javascripts/angular/directives/bibliographyDirective.js
@@ -0,0 +1,10 @@
+titlesApp
+ .directive('bibliography', function() {
+   return {
+     restrict: 'E',
+     transclude: true,
+     scope: {
+     },
+     templateUrl: 'bibliography.html'
+   };
+ });
