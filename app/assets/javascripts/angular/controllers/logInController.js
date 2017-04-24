ff8c9f201142b4ca9fbe986a0f26b22e737061a9
ucilibraries <ucilibraries@users.noreply.github.com>
2017-04-24 11:28:17 -0700

    To fix password reset issue
    To fix password rest, replaced Claire's email with data.email

--- app/assets/javascripts/angular/controllers/logInController.js
+++ app/assets/javascripts/angular/controllers/logInController.js
@@ -2,6 +2,75 @@ titlesApp
   .controller('logInController', ['$scope', 'Auth', '$http', 'userService', function($scope, Auth, $http, userService){
 
   $scope.errors = "";
+titlesApp
+  .controller('logInController', ['$scope', 'Auth', '$http', 'userService', function($scope, Auth, $http, userService){
+
+  $scope.errors = "";
+
+  $scope.passwordReset = false;
+  $scope.logIn = true;
+
+  $scope.passwordResetView = function() {
+    $scope.passwordReset = true;
+    $scope.logIn = false;
+  };
+
+  $scope.logInResetView = function() {
+    $scope.passwordReset = false;
+    $scope.logIn = true;
+  };
+
+  $scope.logInUser = function(data) {
+    var credentials = {
+      email: data.email,
+      password: data.password
+    };
+
+    var config = {
+        headers: {
+            'X-HTTP-Method-Override': 'POST'
+        }
+    };
+
+    Auth.login(credentials, config).then(function(user) {
+      userService.setUser(user);
+    }, function(error) {
+      $scope.errors = error.data.error;
+        // Authentication failed...
+    });
+
+    $scope.$on('devise:login', function(event, currentUser) {
+      // console.log(currentUser, "LOGGED IN");
+    });
+
+    $scope.$on('devise:new-session', function(event, currentUser) {
+      // console.log(currentUser, "NEW SESSION");
+      $scope.errors = "";
+      $scope.dismiss();
+    });
+  };
+
+  $scope.resetPass = function(data, form) {
+    var parameters = {
+      //AC 04112017 email: 'clairewoods@gmail.com'
+      email: data.email
+    };
+
+    Auth.sendResetPasswordInstructions(parameters).then(function(response) {
+        // Sended email if user found otherwise email not sended...
+    }, function(error) {
+        console.log("FAILED", error);
+        if (error.data.errors.email) {
+          $scope.emailExists = true;
+        }
+    });
+
+    $scope.$on('devise:send-reset-password-instructions-successfully', function(event) {
+        // ...
+    });
+  };
+
+}]);
 
   $scope.passwordReset = false;
   $scope.logIn = true;
